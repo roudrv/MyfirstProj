983f454dc08e4193354a218627ba7dad7dd02100
RV <rvpopopk@gmail.com>
2021-12-27 13:33:40 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+"use strict";
+//let number = 10;
+//console.log(number);
+//alert("Hello");
+//const result = confirm("Are you here?");
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answer = [];
+// answer[0] = prompt("Как вас зовут?");
+// answer[1] = prompt("Сколько вам лет?");
+// answer[2] = prompt("Вы женаты?");
+
+// document.write(answer);
+const name = "Rus";
+console.log(`Hello ${name}`);
